5b98ccd8f21be499a65593ed4b730618fbeb162b
neword <newordn@gmail.com>
2019-12-29 13:15:21 +0100

    fixing what is stored into image field on posjt

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,7 +1,7 @@
 
 const post = async (parent,args,context,info)=>{
     const image = await context.storeUpload(args.image)
-    const post = context.prisma.createPost({...args,image})
+    const post = context.prisma.createPost({...args,image:image.path})
     return post
 }
 
